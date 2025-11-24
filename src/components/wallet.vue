95547bbb55c33cf3432371fc4ad5d75617f0e691
Daisy-GonzalezLita12 <22620056@tlaxiaco.tecnm.mx>
2025-11-24 14:45:52 -0600

    first commi11

---
+++ src/components/wallet.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="container mt-5"><form>
+  <div class="text-center">
+    <label style="text-align: center;" for="exampleInputEmail1" class="form-label"><h2> Pagar con la wallet</h2></label>
+   
+  </div>
+               
+  <div class="text-center">
+    <label for="exampleInputPassword1" class="form-label"></label>
+                <button style="text-align: center;" type="button" class="btn btn-primary">Confirmar transacción</button>
+    <p>
+        <strong>Con esta compra has acumulado 12 puntos, los cuales podrás usar en tu siguiente compra</strong>
+    </p>
+  </div>
+
+    
+</form>
+</div>
+</template>
